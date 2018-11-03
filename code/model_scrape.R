4925efbed4f48a3b22ce6ec97647f846c3824f3c
Kiernan Nicholls <k5cents@gmail.com>
2018-11-02 22:20:31 -0400

    Rename code/model_scrape.R

---
+++ code/model_scrape.R
@@ -0,0 +1,62 @@
+# Kiernan Nicholls
+# Fall 2018
+# GOVT-696
+# Collect and format forecast model data
+library(tidyverse)
+
+# read and format senate data
+senate <-
+  read_csv("https://projects.fivethirtyeight.com/congress-model-2018/senate_seat_forecast.csv",
+           col_types =  cols(incumbent = col_logical())) %>%
+  filter(model == "classic",
+         party == "D" | party == "R" | party == "I") %>%
+  rename(date = forecastdate,
+         win.prob = win_probability) %>%
+  mutate(chamber = "senate",
+         voteshare = voteshare / 100,
+         district = "00") %>%
+  unite(col = code,
+        state, district,
+        sep = "-",
+        remove = FALSE) %>%
+  select(date,
+         candidate,
+         chamber,
+         state,
+         district,
+         party,
+         incumbent,
+         voteshare,
+         win.prob)
+
+# read and format house data
+house <-
+  read_csv("https://projects.fivethirtyeight.com/congress-model-2018/house_district_forecast.csv",
+           col_types =  cols(incumbent = col_logical())) %>%
+  filter(model == "classic",
+         party == "D" | party == "R" | party == "I") %>%
+  rename(date = forecastdate,
+         win.prob = win_probability) %>%
+  mutate(chamber = "house",
+         voteshare = voteshare / 100,
+         district = str_pad(district, 2, pad = "0")) %>%
+  unite(col = code,
+        state, district,
+        sep = "-",
+        remove = FALSE) %>%
+  select(date,
+         candidate,
+         chamber,
+         state,
+         district,
+         party,
+         incumbent,
+         voteshare,
+         win.prob)
+
+# combine senate and house
+model.history <- rbind(senate, house)
+model.history <- arrange(model.history, date)
+rm(house, senate)
+
+write_csv(forecast.history, "./data/model_history.csv")

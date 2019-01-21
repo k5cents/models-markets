f81a1453dedd71a81c3e816646824b2be0c7863a
Kiernan Nicholls <k5cents@gmail.com>
2019-01-21 12:33:24 -0500

    Code to add GovTrack stats

--- code/02_members_format.R
+++ code/02_members_format.R
@@ -9,6 +9,7 @@ members <-
   members_115 %>%
   select(last_name,
          govtrack_id,
+         bioguide_id,
          birthday,
          gender,
          type,
@@ -17,7 +18,8 @@ members <-
          party) %>%
   rename(name    = last_name,
          chamber = type,
-         gid     = govtrack_id) %>%
+         gid     = govtrack_id,
+         bid     = bioguide_id) %>%
 
   # Convert the names to ASCII for better cross compatability
   mutate(name = iconv(name, to = "ASCII//TRANSLIT"),
@@ -46,3 +48,16 @@ members <-
 members$code <- str_replace(string = members$code,
                             pattern = "-NA",
                             replacement = "-99")
+
+# Read in the GovTrack idelogy and leadership stats for 115th House and Senate
+members_115_stats <-
+  read_csv("https://web.archive.org/web/20190121171308/https://www.govtrack.us/data/us/115/stats/sponsorshipanalysis_h.txt") %>%
+  bind_rows(read_csv("https://web.archive.org/web/20190121171308/https://www.govtrack.us/data/us/115/stats/sponsorshipanalysis_s.txt")) %>%
+  select(ID, ideology, leadership) %>%
+  rename(gid = ID) %>%
+  mutate(gid = as.character(gid))
+
+# Add stats to frame by GovTrack ID
+members <- left_join(members,
+                     members_115_stats,
+                     by = "gid")

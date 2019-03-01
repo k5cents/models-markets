205dd3a66aafde8d34e1f1becd4b56d815ffbe83
Kiernan Nicholls <kiernann@protonmail.com>
2019-03-01 11:51:45 -0500

    Code to assess predictions

--- code/06_call_format.R
+++ code/06_call_format.R
@@ -1,10 +1,66 @@
-x <-
-  market %>%
+# Kiernan Nicholls
+# Check predictions against election results
+library(tidyverse)
+
+# Some markets only have data on candidate from 1 party
+# We only need probability for one candidate in each race
+# I will use the democratic candidate
+
+# Find races with data on only 1 candidate
+single_party_markets <- market %>%
   group_by(race, date) %>%
   summarise(n = n()) %>%
-  filter(n == 1) # races with only data on 1 candidate
+  filter(n == 1) %>%
+  select(race) %>%
+  as_vector() %>%
+  unique()
 
-y <- market[market$race %in% x$race, ] %>% filter(party != "D")
+# Find races with data on multiple Democratic candidates
+multiple_dems <- model %>%
+  filter(party == "D") %>%
+  group_by(race, date) %>%
+  summarise(n = n()) %>%
+  filter(n > 1) %>%
+  select(race) %>%
+  as_vector() %>%
+  unique()
+
+# Find races with data on multiple Republican candidates
+multiple_gop <- model %>%
+  filter(party == "R") %>%
+  group_by(race, date) %>%
+  summarise(n = n()) %>%
+  filter(n > 1) %>%
+  select(race) %>%
+  as_vector() %>%
+  unique()
+
+# Combine stats for candidates of the party
+model %>%
+  filter(race %in% c(multiple_dems, multiple_gop)) %>%
+  group_by(date, race, party) %>%
+  summarise(prob = sum(prob),
+            min_share = sum(min_share),
+            voteshare = sum(voteshare),
+            max_share = sum(max_share))
+
+# For markets with only Republicans, take the complimentary probability
+# This process assumes all races will only have Republicans or Democrats
+# For the few elections with two candidates of 1 party, leading candidate used
+
+only_gop <-
+  market %>%
+  filter(race %in% single_party_markets,
+         party == "R")
+
+# Invert prices from "Yes" R to "No" R (i.e., "Yes" D)
+gop_only$open  <- 1 - gop_only$open
+gop_only$low   <- 1 - gop_only$low
+gop_only$high  <- 1 - gop_only$high
+gop_only$close <- 1 - gop_only$close
+
+# Invert party from R to D
+gop_only$party <- "D"
 
 y$open  <- 1 - y$open
 y$low   <- 1 - y$low
